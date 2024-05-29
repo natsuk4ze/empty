f6d6018457d7b239fe21d663577d87d42269a03d
natsuk4ze <natsuk4ze@users.noreply.github.com>
2024-05-29 13:18:54 +0000

    deploy: 1948a8ebc9c1729235bbb6d435868362513dcf8a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "333980464"
+    serviceWorkerVersion: "1003008430"
   }
 });
